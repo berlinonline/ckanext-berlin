6803c9cb29d471b2678dae30077fd76ae32b5600
Knud Möller <dunken69@gmail.com>
2013-03-15 15:41:54 +0100

    custom translation for error keys
    to match the different semantics in the Berlin OD-Portal

--- ckanext/berlin/validation.py
+++ ckanext/berlin/validation.py
@@ -12,6 +12,16 @@ log = logging.getLogger(__name__)
 date_pattern = "%Y-%m-%d"
 email_pattern = "^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$"
 
+error_key_mapping = {
+    ('maintainer_email',): (u'Kontakt-Email',),
+    ('date_released',): (u'Veröffentlichungsdatum',),
+    ('date_updated',): (u'Aktualisierungsdatum',),
+    ('temporal_coverage-from',): (u'Zeitraum von',),
+    ('temporal_coverage-to',): (u'Zeitraum bis',),
+    ('author',): (u'Veröffentlichende Stelle',),
+    ('title',): (u'Titel',),
+}
+
 # return True if at least one element of the list data contains a key 'id'
 def at_least_one_id(data):
     for item in data:
@@ -23,31 +33,45 @@ def at_least_one_id(data):
 def not_missing(key, data, errors, context):
 
     value = data.get(key)
+    key = convert_key(key)
     
     if not value or value is missing:
-        errors[key].append(_(u'Wert für ein Pflichtfeld fehlt'))
+        add_error_message(key, errors, u'Wert für ein Pflichtfeld fehlt')
         raise StopOnError
 
 # check if e-mail address seems correct
 def validate_email_address(key, data, errors, context):
 
     value = data.get(key)
+    key = convert_key(key)
     
     if not re.match(email_pattern, value, re.IGNORECASE):
-        errors[key].append(_(u'Dies scheint keine gültige E-Mail-Adresse zu sein'))
+        add_error_message(key, errors, u'Dies scheint keine gültige E-Mail-Adresse zu sein')
         raise StopOnError
 
 # check date format
 def validate_date(key, data, errors, context):
 
     value = data.get(key)
+    key = convert_key(key)
 
     try:
         date_object = datetime.strptime(value, date_pattern)
     except ValueError:
-        errors[key].append(_('Datum bitte im Format JJJJ-MM-TT angeben'))
+        add_error_message(key, errors, u'Datum bitte im Format JJJJ-MM-TT angeben')
         raise StopOnError
 
+def convert_key(key):
+    if key in error_key_mapping:
+        return error_key_mapping[key]
+    else:
+        return key
+
+def add_error_message(key, errors, message):
+    if key in errors:
+        errors[key].append(_(message))
+    else:
+        errors[key] = [(_(message))]
 
 def capture(func, *args, **kwargs):
     """Capture the output of func when called with the given arguments.
